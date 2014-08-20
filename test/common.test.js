2720612fb24e7c4f727826f1fc7b2459d54dc54b
Michiel (SOVEREIGN) <hegemansmichiel@gmail.com>
2014-08-20 16:03:05 +0200

    implemented ipv6 protocol

--- test/common.test.js
+++ test/common.test.js
@@ -1,8 +1,10 @@
-var common = require('./../tools/common');
+var common = require('./../lib/helpers/common');
 var bencode = require('bencode');
 
 var ip = common.hexEncodeIPv4('127.0.0.1', 57895);
+var ipv6 = common.hexEncodeIPv6('fe80::6203:8ff:fea4:9cc', 6881);
 
 console.log(ip);
-
+console.log(ipv6);
 console.log(bencode.encode(new Buffer(ip, 'binary')));
+console.log(bencode.encode(new Buffer(ipv6, 'binary')));
