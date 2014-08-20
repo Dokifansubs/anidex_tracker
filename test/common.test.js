63c8d029e9b886663538fcf4e53ace8211f82e31
Michiel (SOVEREIGN) <hegemansmichiel@gmail.com>
2014-08-20 16:13:29 +0200

    Included ipv6 guards.

--- test/common.test.js
+++ test/common.test.js
@@ -4,7 +4,10 @@ var bencode = require('bencode');
 var ip = common.hexEncodeIPv4('127.0.0.1', 57895);
 var ipv6 = common.hexEncodeIPv6('fe80::6203:8ff:fea4:9cc', 6881);
 
+var ipv62 = common.hexEncodeIPv6('::1:', 6831);
+
 console.log(ip);
 console.log(ipv6);
+console.log(ipv62);
 console.log(bencode.encode(new Buffer(ip, 'binary')));
 console.log(bencode.encode(new Buffer(ipv6, 'binary')));
